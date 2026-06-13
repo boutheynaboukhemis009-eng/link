6df1ef827cf23ed5f470b37b8bf73167d5f86b03
boutheynaboukhemis009-eng <boutheynaboukhemis009@gmail.com>
2026-06-13 19:00:44 +0200

    Update server.js

--- link/backend/server.js
+++ link/backend/server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const multer = require('multer');
 const fs = require('fs');
-
+const response = await fetch(`/user-info?email=${encodeURIComponent(email)}`);
 const app = express();
 
 /**
